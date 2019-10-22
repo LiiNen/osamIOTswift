3bc708ff6884b5c69e05dbe191e51d2259cf9864
osam <osam@osams-iMac.local>
2019-10-22 17:57:00 +0900

    initial commit

---
+++ osamIotApp/mainNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  mainNavigationController.swift
+//  osamIotApp
+//
+//  Created by osam on 22/10/2019.
+//  Copyright © 2019 osam. All rights reserved.
+//
+
+import UIKit
+
+class mainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
